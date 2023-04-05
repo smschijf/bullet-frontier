2ac6245014634c14ebc04b8109c974dcc7c3f4b0
smschijf <210578@student.glu.nl>
2023-04-05 10:00:39 +0200

    wall jumping added

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -242,7 +242,7 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
 
       // turn gravity off while on slope
-      rb.useGravity = !OnSlope();
+      if(!wallrunning) rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
